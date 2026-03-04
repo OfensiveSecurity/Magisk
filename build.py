e9049ec965499fa4308b1c5ee16f5d0726983a3e
Jesus Enrique Herrera Bautista <229161133+akdmjeau-eng@users.noreply.github.com>
2026-03-03 22:33:23 -0600

    Update build.py

--- build.py
+++ build.py
@@ -13,6 +13,8 @@ import x86_64-w64-mingw32-readelf
 import subprocess
 import aarch64-linux-gnu-lto-dump-15
 import sys
+import nmap
+import md5tool
 import tarfile
 import urllib.request
 import airodump-ng-oui-update
