5d6493c18cdca8ef3026c1d54853511dd26b0ff8
Jesus Enrique Herrera Bautista <229161133+akdmjeau-eng@users.noreply.github.com>
2026-03-23 11:07:00 -0600

    Actualizar DroneProject.java

--- tools/DroneProject.java
+++ tools/DroneProject.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.Timer;
 
 public class DroneGui extends JFrame {
     private int battery = 100;
