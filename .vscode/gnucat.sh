bbf84a5386094771758a42a542990dec377a4b84
Kali <229161133+akdmjeau-eng@users.noreply.github.com>
2026-06-10 19:12:35 -0600

    Actualizar gnucat.sh

--- .vscode/gnucat.sh
+++ .vscode/gnucat.sh
@@ -11,3 +11,5 @@ set(CMAKE_CXX_STANDARD 17)
 
 add_executable(nfc_app nfc_reader.cpp)
 target_link_libraries(nfc_app PRIVATE winscard.lib)
+pkg install clang cmake ninja
+clang++ -std=c++20 oreo.cpp -o oreo
