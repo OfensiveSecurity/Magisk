f051f9acbf6a29ef0080fb0afcea4c7775718359
Jesus <229161133+akdmjeau-eng@users.noreply.github.com>
2026-07-15 07:10:00 -0600

    Crear Android.mk

---
+++ .github/workflows/Android.mk
@@ -0,0 +1,9 @@
+# === Added for loc_log.h include path ===
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH)/../utils \
+    $(LOCAL_PATH)/utils
+# Added for auth fixbeat.cpp include usr ===
+STRING_PY_BEFORE +x /
+    @vulkan{
+step~ssh & :0 i3
+   }
